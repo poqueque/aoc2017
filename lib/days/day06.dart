49aab7a1c7c4c4bf80f494ca72c118233379a5ca
Edi <poqueque@yahoo.es>
2022-11-13 22:08:19 +0100

    Day 06

--- lib/days/day06.dart
+++ lib/days/day06.dart
@@ -1,15 +1,61 @@
 
+import 'dart:math';
+
 import '../aoc2017.dart';
 
 class Day06 extends Day {
 
   @override
+  bool get completed => true;
+
+  @override
   part1() {
-    return 0;
+    var data = inputString.split("\t").map((e) => int.parse(e)).toList();
+    List<String> status = [data.join(" ")];
+
+    int cycles = 0;
+    var duplicateDetected = false;
+    while (!duplicateDetected){
+      //redistribute
+      var m = data.reduce(max);
+      var p = data.indexWhere((e) => e == m);
+      data[p] = 0;
+      while (m>0){
+        p++;
+        data[p%data.length]++;
+        m--;
+      }
+      if(status.contains(data.join(" "))) duplicateDetected = true;
+      status.add(data.join(" "));
+      print(data.join(" "));
+      cycles++;
+    }
+    return cycles;
   }
 
   @override
   part2() {
-    return "0";
+
+    var data = inputString.split("\t").map((e) => int.parse(e)).toList();
+    List<String> status = [data.join(" ")];
+
+    int cycles = 0;
+    var duplicateFoundAt = -1;
+    while (duplicateFoundAt<0){
+      //redistribute
+      var m = data.reduce(max);
+      var p = data.indexWhere((e) => e == m);
+      data[p] = 0;
+      while (m>0){
+        p++;
+        data[p%data.length]++;
+        m--;
+      }
+      if(status.contains(data.join(" "))) duplicateFoundAt = status.indexWhere((element) => element == data.join(" "));
+      status.add(data.join(" "));
+      print(data.join(" "));
+      cycles++;
+    }
+    return cycles - duplicateFoundAt;
   }
 }
