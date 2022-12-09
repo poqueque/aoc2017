46ee77c4bc4f06f3e7b1c03d9fe87bdd359af7aa
Edi <poqueque@yahoo.es>
2022-12-09 08:37:37 +0100

    Day 16

--- lib/days/day16.dart
+++ lib/days/day16.dart
@@ -1,15 +1,67 @@
-
 import '../aoc2017.dart';
 
 class Day16 extends Day {
+  @override
+  bool get completed => true;
+  var p = "abcdefghijklmnop";
 
   @override
   part1() {
-    return 0;
+    var steps = inputString.split(",");
+    for (var step in steps) {
+      dance(step);
+    }
+    return p;
   }
 
   @override
   part2() {
-    return "0";
+    p = "abcdefghijklmnop";
+    List<String> d = [];
+    var steps = inputString.split(",");
+    d.add(p);
+
+    bool printed = false;
+    var loop = 1000000000;
+    for (int i = 0; i < loop; i++) {
+      for (var step in steps) {
+        dance(step);
+      }
+      if (d.contains(p)) {
+        var period = i - d.indexOf(p) + 1;
+//        if (!printed) print ("String $p is on position ${d.indexOf(p)} - i: $i - period: $period");
+        var total = d.indexOf(p) + loop % period;
+//        if (!printed) print ("Deberia ser = ${d[total]}");
+        printed = true;
+        return d[total];
+      }
+      d.add(p);
+    }
+    return p;
+  }
+
+  void dance(String step) {
+    if (step[0] == "s") {
+      var moves = int.parse(step.substring(1));
+      p = p.substring(p.length - moves) + p.substring(0, p.length - moves);
+    }
+    if (step[0] == "x") {
+      var data = step.substring(1).split("/");
+      var p1 = int.parse(data[0]);
+      var p2 = int.parse(data[1]);
+      var s1 = p[p1];
+      var s2 = p[p2];
+      p = p.replaceAll(s1, "z");
+      p = p.replaceAll(s2, s1);
+      p = p.replaceAll("z", s2);
+    }
+    if (step[0] == "p") {
+      var data = step.substring(1).split("/");
+      var s1 = data[0];
+      var s2 = data[1];
+      p = p.replaceAll(s1, "z");
+      p = p.replaceAll(s2, s1);
+      p = p.replaceAll("z", s2);
+    }
   }
 }
